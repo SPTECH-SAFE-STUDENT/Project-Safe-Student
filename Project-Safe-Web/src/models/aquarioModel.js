23ebcf274eb2a8c4c9d612ef418f09f15dc162f7
brzenit <bryan.lima@sptech.school>
2024-06-11 20:23:33 -0300

    DashGeral Funcionando as KPIS

--- Project-Safe-Web/src/models/aquarioModel.js
+++ Project-Safe-Web/src/models/aquarioModel.js
@@ -3,32 +3,35 @@ var database = require("../database/config");
 function buscarVansPorEmpresa(fkcnpj) {
   // console.log(usuario.idUsuario)
   var instrucaoSql = `SELECT 
-    v.placa, 
-    v.marca, 
+    v.placa,
+    v.marca,
     v.categoria,
-    s.nome AS sensor_nome,
-    s.localizacao,
-    s.tipo,
-    lt.temperatura AS ultima_temperatura,
-    lt.id AS leitura_id
+    v.qtdBancos,
+    v.statusVan,
+    MAX(lt.temperatura) AS ultima_temperatura
 FROM 
-    Veiculo v
+    veiculo v
 JOIN 
-    Sensores s ON v.placa = s.fkveiculo
-LEFT JOIN 
-    (SELECT 
-         id, temperatura, fksensorTemp
-     FROM 
-         LeituraTemp
-     WHERE 
-         id IN (SELECT MAX(id) FROM LeituraTemp GROUP BY fksensorTemp)
-         ) lt ON s.id = lt.fksensorTemp
-where 
- v.fkCnpj = '${fkcnpj}'
--- fkEmpresa = 1
-and s.tipo = 'temperatura'
-ORDER BY 
-    ultima_temperatura desc;
+    sensores s ON v.placa = s.fkveiculo
+JOIN 
+    LeituraTemp lt ON s.id = lt.fksensorTemp
+WHERE 
+    v.fkCnpj = '${fkcnpj}' AND
+    s.nome IN ('SensorA', 'SensorB') AND
+    lt.id IN (
+        SELECT id
+        FROM (
+            SELECT lt.id
+            FROM LeituraTemp lt
+            JOIN sensores s ON lt.fksensorTemp = s.id
+            WHERE s.fkveiculo = v.placa AND s.nome IN ('SensorA', 'SensorB')
+            ORDER BY lt.id DESC
+            LIMIT 2
+        ) subquery
+    )
+GROUP BY 
+    v.placa, v.marca, v.categoria, v.qtdBancos
+    order by ultima_temperatura desc;
     `;
   // var instrucaoSql = `SELECT * FROM Veiculo where fkEmpresa = '${idUsuario}'`;
 
@@ -39,17 +42,74 @@ ORDER BY
 function buscarVanPorMotorista(idUsuario) {
 
   var instrucaoSql = `select idUsuario, email, cargo, v.placa, qtdBancos, v.fkCnpj , fkUsuario
-from veiculo v
-join usuario on idUsuario = fkUsuario
-where fkUsuario = ${idUsuario};
-      `;
+    from veiculo v
+    join usuario on idUsuario = fkUsuario
+    where fkUsuario = ${idUsuario};
+    `;
 
   console.log("Executando a instrução SQL: \n" + instrucaoSql);
   return database.executar(instrucaoSql);
 
 }
 
+function kpiAlertas(fkCnpj){
 
+  var instrucaoSql = `select 
+count(temperatura) as qtdAlertas
+from veiculo 
+join sensores on fkveiculo = placa
+join leituratemp on fksensorTemp = sensores.id
+where temperatura > 25 
+and fkCnpj = '${fkCnpj}';
+  `;
+
+console.log("Executando a instrução SQL: \n" + instrucaoSql);
+return database.executar(instrucaoSql);
+
+}
+
+function kpiCriticos(fkCnpj){
+
+  var instrucaoSql = `    SELECT 
+          count(lt.temperatura) AS ultima_temperatura
+      FROM 
+          Veiculo v
+      JOIN 
+          Sensores s ON v.placa = s.fkveiculo
+      LEFT JOIN 
+          (SELECT 
+              id, temperatura, fksensorTemp
+          FROM 
+              LeituraTemp
+          WHERE 
+              id IN (SELECT MAX(id) FROM LeituraTemp GROUP BY fksensorTemp)
+              ) lt ON s.id = lt.fksensorTemp
+      where 
+      v.fkCnpj = '${fkCnpj}'
+      -- fkEmpresa = 1
+      and s.tipo = 'temperatura'
+      and lt.temperatura > 28
+      ORDER BY 
+          ultima_temperatura desc;
+  `;
+
+console.log("Executando a instrução SQL: \n" + instrucaoSql);
+return database.executar(instrucaoSql);
+
+}
+
+function kpiVansServico(fkCnpj){
+
+  var instrucaoSql = `
+    select count(statusVan) as VansServico from veiculo
+    where statusVan = 'rodando'
+    and fkCnpj = '${fkCnpj}';
+  `;
+
+console.log("Executando a instrução SQL: \n" + instrucaoSql);
+return database.executar(instrucaoSql);
+
+}
 
 function cadastrar(empresaId, descricao) {
 
@@ -63,5 +123,9 @@ function cadastrar(empresaId, descricao) {
 module.exports = {
   buscarVansPorEmpresa,
   buscarVanPorMotorista,
-  cadastrar
+  cadastrar,
+  kpiCriticos,
+  kpiAlertas,
+  kpiVansServico
+
 }
