23e21a57e4ae85f28584ad9a402e7f4e45378bfd
brzenit <bryan.lima@sptech.school>
2024-06-13 19:21:25 -0300

    Subindo Correção Grafico proximidade

--- Project-Safe-Web/src/models/medidaModel.js
+++ Project-Safe-Web/src/models/medidaModel.js
@@ -15,18 +15,66 @@ function buscarUltimasMedidas(idAquario, limite_linhas) {
     return database.executar(instrucaoSql);
 }
 
-function buscarMedidasEmTempoReal(idAquario) {
+function buscarMedidasEmTempoReal(idVan) {
+    let sensorA = 1
+    let sensorB = 2
 
-    var instrucaoSql = `SELECT 
-        dht11_temperatura as temperatura, 
-        dht11_umidade as umidade,
-                        DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico, 
-                        fk_aquario 
-                        FROM medida WHERE fk_aquario = ${idAquario} 
-                    ORDER BY id DESC LIMIT 1`;
+    if (idVan == 'BAB2222') {
+        sensorA = 17
+        sensorB = 18
+    } else
+        if (idVan == 'AXC1111') {
+            sensorA = 35
+            sensorB = 36
+        }
+
+    var instrucaoSql = `SELECT
+	lt.id,
+    MAX(CASE WHEN lt.fkSensorTemp = ${sensorA} THEN lt.temperatura ELSE NULL END) AS SensorTempA,
+    MAX(CASE WHEN lt.fkSensorTemp = ${sensorB} THEN lt.temperatura ELSE NULL END) AS SensorTempB,
+    lt.horario
+FROM leituraTemp as lt
+INNER JOIN sensores as sn
+    ON lt.fkSensorTemp = sn.id
+WHERE sn.fkVeiculo = '${idVan}'
+GROUP BY lt.id 
+order by lt.id desc
+limit 2;
+    `;
 
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
+}
+
+function buscarTemperatura(idVan) {
+    let sensorA = 1
+    let sensorB = 2
+
+    if (idVan == 'BAB2222') {
+        sensorA = 17
+        sensorB = 18
+    } else
+        if (idVan == 'AXC1111') {
+            sensorA = 35
+            sensorB = 36
+        }
+
+    var buscarTemperaturaID = `SELECT
+	lt.id,
+    MAX(CASE WHEN lt.fkSensorTemp = ${sensorA} THEN lt.temperatura ELSE NULL END) AS SensorTempA,
+    MAX(CASE WHEN lt.fkSensorTemp = ${sensorB} THEN lt.temperatura ELSE NULL END) AS SensorTempB,
+    lt.horario
+FROM leituraTemp as lt
+INNER JOIN sensores as sn
+    ON lt.fkSensorTemp = sn.id
+WHERE sn.fkVeiculo = '${idVan}'
+GROUP BY lt.id 
+order by lt.id desc
+limit 2;
+    `;
+
+    console.log("Executando a instrução SQL: \n" + buscarTemperaturaID);
+    return database.executar(buscarTemperaturaID);
 }
 
 module.exports = {
